0ece89dd0c6afe4f6a6ad25d0676b2dda3c67389
Alisson <alisson.pereira36@etec.sp.gov.br>
2025-03-05 12:42:57 -0300

    80%

--- js/modalHome.js
+++
@@ -1,9 +0,0 @@
-// Ao clicar no botão "Entrar", redireciona para a página de login
-document.getElementById('loginButton').addEventListener('click', function() {
-    window.location.href = './login.html'; // Substitua pelo URL da sua página de login
-});
-
-// Ao clicar no botão "Cadastrar", redireciona para a página de cadastro
-document.getElementById('registerButton').addEventListener('click', function() {
-    window.location.href = './cadastro.html'; // Substitua pelo URL da sua página de cadastro
-});
