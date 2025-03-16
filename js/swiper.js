9bb7fa0048523deee3001229c115fc79c85e4406
Alisson <alisson.pereira36@etec.sp.gov.br>
2025-03-16 20:17:29 -0300

    vai tomando de sequencia

--- js/swiper.js
+++ js/swiper.js
@@ -2,7 +2,7 @@ var swiper = new Swiper('.swiper-container', {
     slidesPerView: 4,          // Exibe 4 slides ao mesmo tempo
     spaceBetween: 20,          // Espaço entre os slides
     slidesPerGroup: 4,         // Avança 4 slides por vez ao clicar nas setas
-    loop: false,               // Desabilita o loop
+    loop: true,               // Desabilita o loop
     centeredSlides: true,      // Centraliza o slide ativo
   
     navigation: {
@@ -36,4 +36,46 @@ var swiper = new Swiper('.swiper-container', {
       },
     },
   });
+
+
+
+  // Inicializando o segundo Swiper (com id swiper2)
+var swiper2 = new Swiper('#swiper2', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  slidesPerGroup: 4,
+  loop: true,
+  centeredSlides: true,
+
+  navigation: {
+    nextEl: '#swiper2 .swiper-button-next',
+    prevEl: '#swiper2 .swiper-button-prev',
+  },
+  pagination: {
+    el: '#swiper2 .swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      centeredSlides: true,
+    },
+    640: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      centeredSlides: false,
+    },
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      centeredSlides: false,
+    },
+    1024: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      centeredSlides: false,
+    },
+  },
+});
   
